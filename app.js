3b0f45db5f113185e4815927647a394f4cfb7b07
Dang Ngoc Tuan <tuandn6597@gmail.com>
2019-05-02 23:09:36 +0700

    memcaced 2

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+//
 // Flash- mess
 app.use(session({
   cookie: { maxAge: 60000*60*60 },
